bab90ff76b06bf116419959ed70f1e8cc4e8176d
sid <32238869+sidharthmirch@users.noreply.github.com>
2021-05-09 02:19:16 +0530

    switched to cryptocompare
    better api
    using currency-symbol-map for symbols

--- main.js
+++ main.js
@@ -1,6 +1,20 @@
+// https://www.npmjs.com/package/cryptocompare
+// fml
+
+
+// want to make a nice format and shit
+// add info in embed
+// use a file with png of coin symbol 
+// use file to pull $ sign etc
+
+const getSymbolFromCurrency = require('currency-symbol-map')
 const Discord = require("discord.js");
-const { prefix, token } = require("./config.json");
+const { prefix, token, apiKey } = require("./config.json");
 const client = new Discord.Client();
+global.fetch = require('node-fetch');
+const cc = require('cryptocompare');
+cc.setApiKey(apiKey);
+let value;
 
 client.on("ready", () => {
   console.log(`${client.user.tag} is now online!`);
@@ -24,47 +38,31 @@ client.on("message", (message) => {
     message.channel.send(
       "`$price [CRYPTO] [CURRENCY]` This will provide the price of a given crypto in the provided currency.\
     \n```Example usage: $price BTC USD --> This would give you the price of BTC in USD.``` \
-    \nLegacy support for `$fbtc` at the moment - will be reworking the function into the new `$price` command. \
-    \n`status` Server count + my discord for help."
+    \n`$status` Provides server count + my discord for help."
     );
   } else if (command === "status") {
     let count = client.guilds.cache.size; 
     message.channel.send(`I am in ${count} servers! 
-    \nIf you want help or would like to report a bug, reach me on discord: \`sid#0003\`! `
-      
-    );
+    \nIf you want help or would like to report a bug, reach me via discord: \`sid#0003\`!`)
   } else if (command === "price") {
-    let price = require("crypto-price");
     let base = args[1];
     let crypto = args[0];
-    price
-      .getCryptoPrice(base, crypto)
-      .then((obj) => {
-        let currentPrice = obj.price;
-        currentPrice = Math.round(currentPrice);
-        message.channel.send(
-          `1 ${crypto.toUpperCase()} is worth ${currentPrice} ${base.toUpperCase()}`
-        );
+      cc.price(crypto, base)
+      .then(prices => {
+        const currentPrice = prices[Object.keys(prices)[0]];
+        // if (crypto.toUpperCase() in getSymbolFromCurrency && base.toUpperCase() in getSymbolFromCurrency) {
+        //     message.channel.send(`${getSymbolFromCurrency(crypto.toUpperCase())}1 is worth ${getSymbolFromCurrency(base.toUpperCase())}${currentPrice}`)   
+        // } else {
+        //     message.channel.send(`1 ${crypto.toUpperCase()} is worth ${currentPrice} ${base.toUpperCase()}`)
+        // }
+        const cryptoSymbol = (getSymbolFromCurrency(crypto.toUpperCase()) !== undefined) ? getSymbolFromCurrency(crypto.toUpperCase()) : crypto.toUpperCase(); 
+        const baseSymbol = (getSymbolFromCurrency(base.toUpperCase()) !== undefined) ? getSymbolFromCurrency(base.toUpperCase()) : base.toUpperCase();
+        message.channel.send(`1${cryptoSymbol} is worth ${baseSymbol}${currentPrice}`)
       })
       .catch((err) => {
-        message.channel.send("That fiat currency is not supported :(");
-      });
-  } else if (command === "fbtc") {
-    let price = require("crypto-price");
-    price
-      .getCryptoPrice("USD", "BTC")
-      .then((obj) => {
-        let currentBtc = obj.price;
-        let fiat = args[0];
-        let amount = fiat / currentBtc;
-        amount = amount.toFixed(8);
-        message.channel.send(
-          `For $${fiat} you would be able to purchase ${amount} BTC`
-        );
+        message.channel.send('The arguements you supplied are causing me to fumble... Please try new values');
       })
-      .catch((err) => {
-        console.log(err);
-      });
+    
   }
 });
 
